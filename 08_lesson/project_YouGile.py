2903ae0e00e8a1bdf83a2e88232347fd7054684d
taha7524 <nenataha.75@gmail.com>
2025-12-18 21:23:14 +0400

    add lesson8

---
+++ 08_lesson/project_YouGile.py
@@ -0,0 +1,69 @@
+import requests
+
+
+class ProjectYouGile:
+    # Инициализация
+    def __init__(self, url) -> None:
+        self.url = url
+
+    # Получить ключ авторизации
+    def get_token(self):
+        payload = {
+            "login": "nenataha.75@gmail.com",
+            "password": "Ucheba2024&",
+            "companyId": "ae49d152-4d9e-4788-aa80-50b11be058e5"
+        }
+        resp = requests.post(self.url + 'auth/keys/get', json=payload)
+        return resp.json()[0]['key']
+
+    # Получить список проектов
+    def get_project_list(self):
+        key = self.get_token()
+        headers = {
+            'Authorization': f'Bearer {key}',
+            'Content-Type': 'application/json'
+        }
+
+        resp = requests.get(self.url + 'projects', headers=headers)
+        return resp.json()['content']
+
+    # Добавить проект:
+    def create_project(self, title, users):
+        key = self.get_token()
+        headers = {
+            'Authorization': f'Bearer {key}',
+            'Content-Type': 'application/json'
+        }
+        body = {
+            "title": title,
+            "users": users
+        }
+        resp = requests.post(self.url + 'projects',
+                             headers=headers,
+                             json=body)
+        return resp.json()
+
+    # Получить проект по id
+    def get_project_with_id(self, project_id):
+        key = self.get_token()
+        headers = {
+            'Authorization': f'Bearer {key}'
+        }
+        resp = requests.get(self.url + f'projects/{project_id}',
+                            headers=headers)
+        return resp.json()
+
+    # Изменить название проекта
+    def edit_project(self, project_id, new_deleted, new_title):
+        key = self.get_token()
+        headers = {
+            'Authorization': f'Bearer {key}',
+            'Content-Type': 'application/json'
+        }
+        project = {
+            "deleted": new_deleted,
+            "title": new_title
+        }
+        resp = requests.put(self.url + f'projects/{project_id}',
+                            headers=headers, json=project)
+        return resp.json()
